fe79d68762820db19f9329f04600cef0b519c3dd
tmp-lemon <fnleonar@protonmail.com>
2025-07-12 01:39:33 +0000

    add htmlnode.py

---
+++ src/htmlnode.py
@@ -0,0 +1,18 @@
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError()
+
+    def props_to_html(self):
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -5,25 +5,29 @@ from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node1 = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
-        self.assertEqual(node1, node2)
-    
-    def test_eq_false(self):
-        node1 = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a different text node", TextType.LINK, "https://www.testing.com")
-        self.assertNotEqual(node1, node2)
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.TEXT)
+        self.assertEqual(node, node2)
 
-    def test_eq_link(self):
-        node1 = TextNode("This is a text node", TextType.LINK, "https://www.testing.com")
-        node2 = TextNode("This is a text node", TextType.LINK, "https://www.testing.com")
-        self.assertEqual(node1, node2)
+    def test_eq_false(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node2", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        self.assertEqual(node, node2)
 
     def test_repr(self):
-        node = TextNode("This is a text node", TextType.LINK, "https://www.testing.com")
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
         self.assertEqual(
-            "TextNode(This is a text node, link, https://www.testing.com)",
-            repr(node)
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
 
 
